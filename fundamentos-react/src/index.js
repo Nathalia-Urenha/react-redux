740ab6d74e5ea422ec6d5932e45138c6034977f1
Nathalia Urenha <52292944+Nathalia-Urenha@users.noreply.github.com>
2022-04-23 14:50:44 -0300

    basic components

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <div>
+        <Primeiro />
+        <ComParametro 
+            titulo="Situação do aluno" 
+            nota={10} 
+            aluno="Nathália" 
+        />
+        <ComParametro 
+            titulo="Situação do aluno" 
+            nota={9} 
+            aluno="Igor" 
+        />
+    </div>,
+    document.getElementById('root')
+)
